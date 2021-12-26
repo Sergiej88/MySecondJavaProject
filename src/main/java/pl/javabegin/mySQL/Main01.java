247fa18ee09905e5d87caa0d688ab57710fae096
Sergiej88 <sergey88gavrilik@gmail.com>
2021-12-26 22:49:54 +0100

    update2

--- src/main/java/pl/javabegin/mySQL/Main01.java
+++ src/main/java/pl/javabegin/mySQL/Main01.java
@@ -1,4 +1,14 @@
 package pl.javabegin.mySQL;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class Main01 {
+    public static void main(String[] args) {
+        try(Connection conn = DBUtil.connect()) {
+            System.out.println("Connect successful");
+        }catch (SQLException e){
+            System.out.println("Oooopppssss");
+        }
+    }
 }
